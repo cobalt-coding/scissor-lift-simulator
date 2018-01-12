1d4a0536a8b748c939e9608987359848c9d577c4
Junha Lee <junhalee02@gmail.com>
2018-01-11 16:33:28 -0800

    Added basic platforming

---
+++ Scissor Lift Simulator/Assets/Scripts/RobotController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+    
+    public Rigidbody2D rb;
+    public float force = 50;
+    public float jumpForce = 500;
+
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*force, 0)*Time.deltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce)*Time.deltaTime);
+            Debug.Log("you pressed jump!");
+        }
+	}
+}
