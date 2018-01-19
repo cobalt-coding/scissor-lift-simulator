cd39d07f02ad0ed5295adfe757e0b20811ca1003
StormSteel <ds332a@outlook.com>
2018-01-18 16:09:39 -0800

    Did a thing
    also jumping is fixed

--- Scissor Lift Simulator/Assets/Scripts/RobotController.cs
+++ Scissor Lift Simulator/Assets/Scripts/RobotController.cs
@@ -7,15 +7,36 @@ public class RobotController : MonoBehaviour {
     public Rigidbody2D rb;
     public float force = 50;
     public float jumpForce = 500;
+    private bool grounded = false;
 
 	// Update is called once per frame
 	void Update () {
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*force, 0)*Time.deltaTime);
 
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && grounded == true)
         {
             rb.AddForce(new Vector2(0, jumpForce)*Time.deltaTime);
             Debug.Log("you pressed jump!");
         }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+           if (collision.gameObject.tag == "Ground")
+            {
+            grounded = true;
+            }
+      
+       
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+
+
+    }
 }
