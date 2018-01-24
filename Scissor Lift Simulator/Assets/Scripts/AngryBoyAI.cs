2a0b31616b0e4a094b6c7d05e167dc2b514ea37e
Jakob Stickles <theawsomejs@gmail.com>
2018-01-23 19:26:11 -0800

    Angry Boy is Smart Boi

--- Scissor Lift Simulator/Assets/Scripts/AngryBoyAI.cs
+++ Scissor Lift Simulator/Assets/Scripts/AngryBoyAI.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AngryBoyAI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Rigidbody2D rb;
+    public bool right = true;
+    public float force = 2000;
+    int delay;
+
+    // Use this for initialization
+    void Start() {
+        new Vector2(force, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (right) 
+            rb.AddForce(new Vector2(force, 0) * Time.deltaTime);
+        else
+            rb.AddForce(new Vector2(-force, 0) * Time.deltaTime);
+
+        
+        if (Math.Abs(rb.velocity.x) < 1 && Time.frameCount - delay > 10)
+        {
+            right = !right;
+            delay = Time.frameCount;
+        }
 	}
 }
