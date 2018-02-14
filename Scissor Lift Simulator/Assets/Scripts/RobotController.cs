0e453beb04183a6e26b1d4fc738419571fb02ba5
Junha Lee <junhalee02@gmail.com>
2018-02-13 18:35:10 -0800

    Jumping is no longer inconsistant

--- Scissor Lift Simulator/Assets/Scripts/RobotController.cs
+++ Scissor Lift Simulator/Assets/Scripts/RobotController.cs
@@ -65,7 +65,7 @@ public class RobotController : MonoBehaviour {
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         switch(collision.gameObject.tag)
         {
