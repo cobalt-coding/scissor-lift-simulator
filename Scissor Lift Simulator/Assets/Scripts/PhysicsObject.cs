7d37f883685975192ac5a460eb847c3f25a398a8
Jared Swislow <jayswis2002@gmail.com>
2018-04-12 22:37:57 -0700

    Gravity
    Not instead of Unity's gravity, we have gravity that was NOT copy and pasted from here: https://unity3d.com/learn/tutorials/topics/2d-game-creation/scripting-gravity?playlist=17093

---
+++ Scissor Lift Simulator/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+
+    void onEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate ()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+
+        Movement(move);
+    }
+
+    void Movement(Vector2 move)
+    {
+        rb2d.position += move;
+    }
+
+}
